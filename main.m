3dc631377ab4e11917847ac02208a813034b4d38
TsingPig <1114196607@qq.com>
2023-12-15 11:03:57 +0800

    图像分割

--- main.m
+++ main.m
@@ -1,6 +1,29 @@
-clc; % 清除命令窗口内容
-close all; % 关闭所有图形窗口
-clear all; % 清空所有变量e
-addpath("函数");
-%%
-a = 1
+clc;
+close all;
+clear all;
+
+% 读入图像和真实分割
+predictedSegmentation = imread('自动扣.png');
+groundTruth = imread('手动扣.png');
+
+% 将图像转换为二进制图像（如果不是的话）
+predictedBinary = im2bw(predictedSegmentation);
+groundTruthBinary = im2bw(groundTruth);
+
+% 调整图像大小，使其具有相同的大小
+[height, width] = size(predictedBinary);
+groundTruthBinary = imresize(groundTruthBinary, [height, width]);
+
+% 计算交集和并集
+intersection = predictedBinary & groundTruthBinary;
+union = predictedBinary | groundTruthBinary;
+
+% 计算每个类别的交并比
+classIoU = sum(intersection(:)) / sum(union(:));
+
+% 计算平均交并比
+numClasses = max([predictedBinary(:); groundTruthBinary(:)]);
+meanIoU = sum(classIoU) / numClasses;
+
+% 显示结果
+disp(['Mean Intersection over Union (MIoU): ' num2str(meanIoU)]);
