4865abe2aaf2f9bfdce3c45ebb333317fb892f02
TsingPig <1114196607@qq.com>
2023-12-15 13:43:31 +0800

    图像分割推进：UI，实现最大类间方差法Ostu实现图像分割

--- main.m
+++ main.m
@@ -2,6 +2,50 @@ clc;
 close all;
 clear all;
 
+
+
+%%
+% 读入包含叶子和背景的彩色图片
+image = imread('63.png');  % 替换为你的图片路径
+
+% 显示原始图片
+figure;
+imshow(image);
+title('Original Image');
+
+% 转换为灰度图像
+grayImage = rgb2gray(image);
+
+% 使用阈值分割方法（可根据实际情况调整阈值）
+threshold = graythresh(grayImage);
+binaryImage = imbinarize(grayImage, threshold);
+
+% 对二值图像进行后处理（去除小的区域）
+binaryImage = bwareaopen(binaryImage, 1000);
+
+% 显示分割的二值图像
+figure;
+imshow(binaryImage);
+title('Binary Segmentation Image');
+
+% 创建彩色蒙版图像
+colorMask = ~cat(3, binaryImage, binaryImage, binaryImage);
+
+
+
+% 将彩色蒙版叠加到原始图片上
+segmentedResult = image;
+% 获取满足条件的像素索引
+indices = find(colorMask);
+% 将这些像素设置为白色
+segmentedResult(indices) = 255;
+
+% 显示彩色蒙版图像
+figure;
+imshow(segmentedResult);
+title('Colored Segmentation Result');
+
+%%
 % 读入图像和真实分割
 predictedSegmentation = imread('自动扣.png');
 groundTruth = imread('手动扣.png');
